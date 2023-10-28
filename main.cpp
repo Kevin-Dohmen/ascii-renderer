bf294da4f68a35857fc0be42e6f121dd810a00da
Kevin Dohmen <kevindohmen06@gmail.com>
2023-10-28 11:14:39 +0200

    i thought i pushed this already...

--- main.cpp
+++ main.cpp
@@ -145,7 +145,7 @@ float shader(vec2 fragpos, vec2 fragres, float cTime, int cFrame)
     float sprdist = (uv - sphr).length();
 
     if(sprdist <= sr){
-        col = 1.0f - sprdist / sr;
+        col = 1.0f - (sprdist / sr);
     }
     else{
         col = 0.f;
