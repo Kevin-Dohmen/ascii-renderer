d85f8f88b54bd0ffb58b5a82d08df645be4940ae
Kevin Dohmen <kevindohmen06@gmail.com>
2023-11-05 15:08:05 +0100

    color

--- test/simple ray tracer/main.cpp
+++ test/simple ray tracer/main.cpp
@@ -20,9 +20,9 @@ public:
         return vec2(x - other.x, y - other.y);
     }
 
-    vec2 operator*(float scalar) const
+    vec2 operator*(vec2 scalar) const
     {
-        return vec2(x * scalar, y * scalar);
+        return vec2(x * scalar.x, y * scalar.y);
     }
 
     vec2 operator/(vec2 scalar) const
@@ -102,21 +102,24 @@ public:
     }
 };
 
-const int resx = 1000;
-const int resy = 1000;
+const int resx = 500;
+const int resy = 500;
+const int colamnt = 3;
 const vec2 resolution = vec2(resx, resy);
 
-int writefile(float image[resx][resy]) {
+int writefile(float image[resx][resy][colamnt]) {
     std::ofstream file;
     file.open("image.data");
     for (int y = 0; y < resy; y++) {
         for (int x = 0; x < resx; x++) {
-            float val = image[x][y];
-            if(val >= 0.0f || val <= 1.0f){
-                file << (int8_t)(image[x][y]*256.0f);
-            } else {
-                std::cout << "Error: Pixel value out of range: " << image[x][y] << std::endl;
-                file << (int8_t)0;
+            for (int c = 0; c < colamnt; c++){
+                // std::cout << x << " " << y << " " << c << std::endl;
+                float val = image[x][y][c];
+                if(val >= 0.0f && val <= 1.0f){
+                    file << (int8_t)(val*256.0f);
+                } else {
+                    file << (int8_t)0;
+                }
             }
         }
     }
@@ -124,19 +127,25 @@ int writefile(float image[resx][resy]) {
     return 0;
 }
 
-float shader(vec2 fragres, vec2 fragpos){
-    float col = 0.0f;
-    vec2 uv = (fragpos / fragres) * 10;
-    col = uv.length();
+vec3 shader(vec2 fragres, vec2 fragpos){
+    vec3 col = vec3(0.0f, 0.0f, 0.0f);
+    vec2 uv = (fragpos / fragres);
+    col.x = uv.x;
+    col.y = uv.y;
+    col.z = uv.length();
+    // std::cout << col.y << std::endl;
     // std::cout << uv.x << " " << uv.y << " " << uv.length() << std::endl;
     return col;
 }
-
+ 
 int main() {
-    float image[resx][resy];
+    float image[resx][resy][colamnt];
     for(int x = 0; x < resx; x++){
         for(int y = 0; y < resy; y++){
-            image[x][y] = shader(resolution, vec2(x, y));
+            vec3 col = shader(resolution, vec2(x, y));
+            image[x][y][0] = col.x;
+            image[x][y][1] = col.y;
+            image[x][y][2] = col.z;
         }
     }
 
